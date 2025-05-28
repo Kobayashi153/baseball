3ec677769fb8855ff8718ae2171a144f31de1853
Kobayashi153 <hahaharu.enjoyhahaha@gmail.com>
2025-05-28 09:09:21 +0900

    Add files via upload

---
+++ npb.js
@@ -0,0 +1,12 @@
+ document.getElementById('myButton').addEventListener('click', function() {
+    // 遷移先のHTMLファイルのURLを指定
+    location.replace('seleague.html');
+    // 別のページ履歴を残したい場合はlocation.hrefを使用
+    // location.href = '別のHTMLファイル.html';
+  });
+ document.getElementById('youButton').addEventListener('click', function() {
+    // 遷移先のHTMLファイルのURLを指定
+    location.replace('paleague.html');
+    // 別のページ履歴を残したい場合はlocation.hrefを使用
+    // location.href = '別のHTMLファイル.html';
+  });
